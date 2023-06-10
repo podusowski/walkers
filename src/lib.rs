ec10d323e514fc4472fc8aec7375f0708569cbf9
Piotr Podusowski <podusowski@gmail.com>
2023-06-10 18:02:28 +0200

    zoom mod

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,9 @@
 mod map;
 mod mercator;
 mod tiles;
+mod zoom;
 
-pub use map::{Map, MapCenterMode, MapMemory, Zoom};
+pub use map::{Map, MapCenterMode, MapMemory};
 pub use mercator::{Position, PositionExt};
 pub use tiles::Tiles;
+pub use zoom::Zoom;
--- src/map.rs
+++ src/map.rs
@@ -1,13 +1,10 @@
-use std::{
-    collections::{hash_map::Entry, HashMap},
-    ops::Deref,
-};
+use std::collections::{hash_map::Entry, HashMap};
 
 use egui::{Mesh, Painter, Pos2, Response, Sense, Ui, Widget};
 
 use crate::{
     mercator::{screen_to_position, PositionExt, TileId},
-    Position, Tiles,
+    Position, Tiles, Zoom,
 };
 
 /// Slippy map widget.
@@ -104,82 +101,6 @@ impl Default for MapMemory {
         }
     }
 }
-
-#[derive(thiserror::Error, Debug, PartialEq, Eq)]
-#[error("invalid zoom level")]
-pub struct InvalidZoom;
-
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
-pub struct Zoom(u8);
-
-impl TryFrom<u8> for Zoom {
-    type Error = InvalidZoom;
-
-    fn try_from(value: u8) -> Result<Self, Self::Error> {
-        // OSM wiki has level 20 listed https://wiki.openstreetmap.org/wiki/Zoom_levels,
-        // but when requested, server responds with 400: Bad Request.
-        if value > 19 {
-            Err(InvalidZoom)
-        } else {
-            Ok(Self(value))
-        }
-    }
-}
-
-impl Default for Zoom {
-    fn default() -> Self {
-        Self(16)
-    }
-}
-
-impl Deref for Zoom {
-    type Target = u8;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
-    }
-}
-
-impl Zoom {
-    pub fn zoom_in(&mut self) -> Result<(), InvalidZoom> {
-        *self = Self::try_from(self.0 + 1)?;
-        Ok(())
-    }
-
-    pub fn zoom_out(&mut self) -> Result<(), InvalidZoom> {
-        *self = Self::try_from(self.0.checked_sub(1).ok_or(InvalidZoom)?)?;
-        Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_constructing_zoom() {
-        assert_eq!(16, *Zoom::default());
-        assert_eq!(19, *Zoom::try_from(19).unwrap());
-        assert_eq!(Err(InvalidZoom), Zoom::try_from(20));
-    }
-
-    #[test]
-    fn test_zooming_in() {
-        let mut zoom = Zoom::try_from(18).unwrap();
-        assert!(zoom.zoom_in().is_ok());
-        assert_eq!(19, *zoom);
-        assert_eq!(Err(InvalidZoom), zoom.zoom_in());
-    }
-
-    #[test]
-    fn test_zooming_out() {
-        let mut zoom = Zoom::try_from(1).unwrap();
-        assert!(zoom.zoom_out().is_ok());
-        assert_eq!(0, *zoom);
-        assert_eq!(Err(InvalidZoom), zoom.zoom_out());
-    }
-}
-
 fn draw_tiles(
     painter: &Painter,
     tile_id: TileId,
