dc20a9b337f99d9e1e122031fb1f6bc85c37b9b2
Piotr Podusowski <podusowski@gmail.com>
2023-06-11 14:29:24 +0200

    some comments added

--- examples/osm.rs
+++ examples/osm.rs
@@ -1,5 +1,5 @@
-use egui::{Align2, RichText, Window};
-use walkers::{Map, MapMemory, Position, Tiles};
+use egui::{Align2, Color32, Painter, RichText, Shape, Ui, Vec2, Window};
+use walkers::{Map, MapMemory, Position, PositionExt, Tiles};
 
 fn main() -> Result<(), eframe::Error> {
     env_logger::init();
@@ -32,6 +32,50 @@ fn wroclaw_glowny() -> Position {
     Position::new(17.03664, 51.09916)
 }
 
+/// Taking a public bus (line 106) is probably the cheapest option to get from
+/// the train station to the airport.
+/// https://www.wroclaw.pl/en/how-and-where-to-buy-public-transport-tickets-in-wroclaw
+fn dworcowa_bus_stop() -> Position {
+    Position::new(17.03940, 51.10005)
+}
+
+fn draw_custom_shapes(ui: &Ui, painter: Painter, map_memory: &MapMemory, my_position: Position) {
+    // Geographical position of the point we want to put our shapes.
+    let position = dworcowa_bus_stop();
+
+    // Turn that into a flat, mercator projection.
+    let projected_position = position.project_with_zoom(*map_memory.zoom);
+
+    // We also need to know where the map center is.
+    let map_center_projected_position = map_memory
+        .center_mode
+        .position(my_position)
+        .project_with_zoom(*map_memory.zoom);
+
+    // From the two points above we can calculate the actual point on the screen.
+    let screen_position = painter.clip_rect().center()
+        + (Vec2::from(projected_position) - Vec2::from(map_center_projected_position));
+
+    // Now we can just use Painter to draw stuff.
+    let text_background = |text: &Shape| {
+        let bg = text.visual_bounding_rect().expand(5.);
+        Shape::rect_filled(bg, 5., ui.visuals().extreme_bg_color)
+    };
+
+    let text = ui.fonts(|fonts| {
+        Shape::text(
+            fonts,
+            screen_position,
+            Align2::LEFT_CENTER,
+            "⬉ Here you can board the 106 line\nwhich goes to the airport.",
+            Default::default(),
+            ui.visuals().text_color(),
+        )
+    });
+    painter.add(text_background(&text));
+    painter.add(text);
+}
+
 impl eframe::App for Osm {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
         egui::CentralPanel::default().show(ctx, |ui| {
@@ -42,11 +86,17 @@ impl eframe::App for Osm {
                 ui.hyperlink("https://donate.openstreetmap.org/");
             });
 
-            ui.add(Map::new(
+            let my_position = wroclaw_glowny();
+
+            let response = ui.add(Map::new(
                 &mut self.tiles,
                 &mut self.map_memory,
                 wroclaw_glowny(),
             ));
+
+            // Draw custom shapes.
+            let painter = ui.painter().with_clip_rect(response.rect);
+            draw_custom_shapes(ui, painter, &self.map_memory, my_position);
 
             Window::new("Map")
                 .collapsible(false)
