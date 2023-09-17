a0257b5187be13502d8ebb86120d60182915e992
Piotr Podusowski <podusowski@gmail.com>
2023-09-17 13:07:07 +0200

    use more modern eframe fn

---
+++ demo_web/src/main.rs
@@ -0,0 +1,17 @@
+fn main() {
+    // Redirect `log` message to `console.log` and friends:
+    eframe::WebLogger::init(log::LevelFilter::Debug).ok();
+
+    let web_options = eframe::WebOptions::default();
+
+    wasm_bindgen_futures::spawn_local(async {
+        eframe::WebRunner::new()
+            .start(
+                "the_canvas_id", // hardcode it
+                web_options,
+                Box::new(|cc| Box::new(demo::MyApp::new(cc))),
+            )
+            .await
+            .expect("failed to start eframe");
+    });
+}
