a55c6ab2a4b0def0492e4a085a643911bf8d50fe
Piotr Podusowski <podusowski@gmail.com>
2023-10-25 20:39:53 +0200

    fmt

--- walkers/src/lib.rs
+++ walkers/src/lib.rs
@@ -1,6 +1,7 @@
 #![doc = include_str!("../README.md")]
 #![deny(clippy::unwrap_used, rustdoc::broken_intra_doc_links)]
 
+mod download;
 pub mod extras;
 mod io;
 mod map;
@@ -8,7 +9,6 @@ mod mercator;
 pub mod providers;
 mod tiles;
 mod zoom;
-mod download;
 
 pub use map::{Center, Map, MapMemory, Plugin, Projector};
 pub use mercator::{screen_to_position, Position, PositionExt};
