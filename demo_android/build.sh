44e50a48a5a905e871a1d69d098f72d8b58b0269
Piotr Podusowski <podusowski@gmail.com>
2023-12-10 23:09:15 +0100

    change directory

--- demo_android/build.sh
+++ demo_android/build.sh
@@ -1,5 +1,8 @@
 set -e
 
+trap popd EXIT
+pushd $(dirname $0)
+
 cargo ndk --target arm64-v8a -o app/src/main/jniLibs/ build --profile release
 ./gradlew installDebug
 adb shell am start -n local.walkers.debug/local.walkers.MainActivity
