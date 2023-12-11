a3feb9233d580bb57c0d23fc2f2c442ebeb1e771
Piotr Podusowski <podusowski@gmail.com>
2023-12-11 17:43:59 +0100

    removed unused Java imports

--- demo_android/app/src/main/java/local/walkers/MainActivity.java
+++ demo_android/app/src/main/java/local/walkers/MainActivity.java
@@ -1,39 +1,6 @@
 package local.walkers;
 
-import android.Manifest;
-import android.content.ClipData;
-import android.content.ClipboardManager;
-import android.content.ComponentName;
-import android.content.Context;
-import android.content.Intent;
-import android.content.ServiceConnection;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
-import android.location.Location;
-import android.os.Build.VERSION;
-import android.os.Build.VERSION_CODES;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.util.Log;
-import android.view.View;
-import android.view.WindowManager;
-
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
-import androidx.core.content.ContextCompat;
-import androidx.core.view.WindowCompat;
-import androidx.core.view.WindowInsetsCompat;
-import androidx.core.view.WindowInsetsControllerCompat;
-
-import com.google.android.gms.location.LocationCallback;
-import com.google.android.gms.location.LocationResult;
 import com.google.androidgamesdk.GameActivity;
-
-import java.util.Arrays;
 
 public class MainActivity extends GameActivity {
   static {
