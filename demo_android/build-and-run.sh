593ba8afe41110fe4fd70cb1754c8097f0850863
Piotr Podusowski <podusowski@gmail.com>
2023-12-14 18:18:24 +0100

    fix paths

--- demo_android/build-and-run.sh
+++ demo_android/build-and-run.sh
@@ -3,6 +3,9 @@ set -e
 trap popd EXIT
 pushd demo_android/
 
-cargo ndk --target arm64-v8a -o app/src/main/jniLibs/ build --profile release
+cd rust
+cargo ndk --target arm64-v8a -o ../java/app/src/main/jniLibs/ build --profile release
+
+cd ../java
 ./gradlew installDebug
 adb shell am start -n local.walkers/.MainActivity
