RustWorks/walkers-map-branches

make Texture and TileId public to allow to build new Tile sources

walkers/src/lib.rs
@@ -11,6 +11,6 @@ mod tiles;
 mod zoom;
 
 pub use map::{Map, MapMemory, Plugin, Projector};
-pub use mercator::{screen_to_position, Position};
-pub use tiles::{Tiles, TilesManager};
+pub use mercator::{screen_to_position, Position, TileId};
+pub use tiles::{Tiles, TilesManager, Texture};
 pub use zoom::InvalidZoom;

podusowski/walkers

walkers/src/lib.rs
@@ -11,6 +11,6 @@ mod tiles;
 mod zoom;
 
 pub use map::{Map, MapMemory, Plugin, Projector};
-pub use mercator::{screen_to_position, Position};
-pub use tiles::{Tiles, TilesManager};
+pub use mercator::{screen_to_position, Position, TileId};
+pub use tiles::{Texture, Tiles, TilesManager};
 pub use zoom::InvalidZoom;
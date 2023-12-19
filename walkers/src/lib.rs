22f9f327d66a5feff48887d9aca993238bfeb2c5
Noktoborus <noktoborus@gmail.com>
2023-12-19 18:00:26 +0400

    make Texture and TileId public to allow to build new Tile sources

--- walkers/src/lib.rs
+++ walkers/src/lib.rs
@@ -11,6 +11,6 @@ mod tiles;
 mod zoom;
 
 pub use map::{Map, MapMemory, Plugin, Projector};
-pub use mercator::{screen_to_position, Position};
-pub use tiles::{Tiles, TilesManager};
+pub use mercator::{screen_to_position, Position, TileId};
+pub use tiles::{Texture, Tiles, TilesManager};
 pub use zoom::InvalidZoom;
