5bd66fcac45bc3837be1db0f9a7bf13355ad91c3
Piotr Podusowski <podusowski@gmail.com>
2024-01-14 22:43:05 +0100

    bind fn

--- hypermocker/src/lib.rs
+++ hypermocker/src/lib.rs
@@ -1,5 +1,10 @@
 use std::{
-    collections::HashMap, convert::Infallible, future::Future, net::SocketAddr, pin::Pin, sync::Arc,
+    collections::HashMap,
+    convert::Infallible,
+    future::Future,
+    net::{SocketAddr, SocketAddrV4},
+    pin::Pin,
+    sync::Arc,
 };
 
 use http_body_util::Full;
@@ -21,12 +26,42 @@ struct State {
     requests: HashMap<String, tokio::sync::oneshot::Sender<Bytes>>,
 }
 
-struct Mock {
+pub struct Mock {
     port: u16,
     state: Arc<Mutex<State>>,
 }
 
 impl Mock {
+    pub async fn bind() -> Result<Mock, Box<dyn std::error::Error + Send + Sync>> {
+        let state = Arc::new(Mutex::new(State::default()));
+
+        let addr = SocketAddr::from(([127, 0, 0, 1], 0));
+        let listener = TcpListener::bind(addr).await.unwrap();
+        let port = listener.local_addr().unwrap().port();
+
+        let state_clone = state.clone();
+        tokio::spawn(async move {
+            loop {
+                let (stream, _) = listener.accept().await.unwrap();
+                let io = TokioIo::new(stream);
+
+                let state = state_clone.clone();
+                tokio::task::spawn(async move {
+                    // Finally, we bind the incoming connection to our `hello` service
+                    if let Err(err) = http1::Builder::new()
+                        // `service_fn` converts our function in a `Service`
+                        .serve_connection(io, MockRequest { state })
+                        .await
+                    {
+                        println!("Error serving connection: {:?}", err);
+                    }
+                });
+            }
+        });
+
+        Ok(Mock { port, state })
+    }
+
     pub async fn expect(&self, url: String) -> Expectation {
         log::info!("Expecting '{}'.", url);
 
@@ -93,49 +128,19 @@ impl Service<Request<hyper::body::Incoming>> for MockRequest {
     }
 }
 
-async fn start_mock() -> Result<Mock, Box<dyn std::error::Error + Send + Sync>> {
-    let state = Arc::new(Mutex::new(State::default()));
-
-    let addr = SocketAddr::from(([127, 0, 0, 1], 0));
-    let listener = TcpListener::bind(addr).await.unwrap();
-    let port = listener.local_addr().unwrap().port();
-
-    let state_clone = state.clone();
-    tokio::spawn(async move {
-        loop {
-            let (stream, _) = listener.accept().await.unwrap();
-            let io = TokioIo::new(stream);
-
-            let state = state_clone.clone();
-            tokio::task::spawn(async move {
-                // Finally, we bind the incoming connection to our `hello` service
-                if let Err(err) = http1::Builder::new()
-                    // `service_fn` converts our function in a `Service`
-                    .serve_connection(io, MockRequest { state })
-                    .await
-                {
-                    println!("Error serving connection: {:?}", err);
-                }
-            });
-        }
-    });
-
-    Ok(Mock { port, state })
-}
-
 #[cfg(test)]
 mod tests {
     use std::time::Duration;
 
     use hyper::body::Bytes;
 
-    use crate::start_mock;
+    use crate::Mock;
 
     #[tokio::test]
     async fn expectation_then_request() {
         let _ = env_logger::try_init();
 
-        let mock = start_mock().await.unwrap();
+        let mock = Mock::bind().await.unwrap();
         let url = format!("http://localhost:{}/foo", mock.port);
         let request = mock.expect("/foo".to_string()).await;
 
@@ -159,7 +164,7 @@ mod tests {
     async fn request_then_expectation() {
         let _ = env_logger::try_init();
 
-        let mock = start_mock().await.unwrap();
+        let mock = Mock::bind().await.unwrap();
         let url = format!("http://localhost:{}/foo", mock.port);
 
         futures::future::join(
