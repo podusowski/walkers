1dd6d0c223596b4190aeeb79415bb7e887ce7a58
Piotr Podusowski <podusowski@gmail.com>
2024-04-01 14:52:45 +0200

    expose hyper

--- hypermocker/src/lib.rs
+++ hypermocker/src/lib.rs
@@ -14,6 +14,7 @@ use tokio::net::TcpListener;
 use tokio::sync::oneshot;
 
 pub use hyper::body::Bytes;
+pub use hyper;
 
 type Response = hyper::Response<Full<Bytes>>;
 pub type StatusCode = hyper::StatusCode;
