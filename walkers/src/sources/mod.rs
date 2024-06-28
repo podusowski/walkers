7c644fc827745c81168a4df5f757385aca278e27
Piotr Podusowski <podusowski@gmail.com>
2024-06-28 22:07:32 +0200

    geoportal

--- walkers/src/sources/mod.rs
+++ walkers/src/sources/mod.rs
@@ -1,6 +1,7 @@
 //! Some common HTTP tile sources. Make sure you follow terms of usage of the particular source.
 
 mod openstreetmap;
+mod geoportal;
 
 use crate::mercator::TileId;
 pub use openstreetmap::OpenStreetMap;
@@ -20,36 +21,6 @@ pub trait TileSource {
     /// Size of each tile, should be a multiple of 256
     fn tile_size(&self) -> u32 {
         256
-    }
-}
-
-/// Orthophotomap layer from Poland's Geoportal.
-/// <https://www.geoportal.gov.pl/uslugi/usluga-przegladania-wms>
-pub struct Geoportal;
-
-impl TileSource for Geoportal {
-    fn tile_url(&self, tile_id: TileId) -> String {
-        format!(
-            "https://mapy.geoportal.gov.pl/wss/service/PZGIK/ORTO/WMTS/StandardResolution?\
-            &SERVICE=WMTS\
-            &REQUEST=GetTile\
-            &VERSION=1.0.0\
-            &LAYER=ORTOFOTOMAPA\
-            &TILEMATRIXSET=EPSG:3857\
-            &TILEMATRIX=EPSG:3857:{}\
-            &TILEROW={}\
-            &TILECOL={}",
-            tile_id.zoom, tile_id.y, tile_id.x
-        )
-    }
-
-    fn attribution(&self) -> Attribution {
-        Attribution {
-            text: "Główny Urząd Geodezji i Kartografii",
-            url: "https://www.geoportal.gov.pl/",
-            logo_light: None,
-            logo_dark: None,
-        }
     }
 }
 
