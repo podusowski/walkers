d1d19b93df7a397ca8962f6de21824c4c027981b
Piotr Podusowski <podusowski@gmail.com>
2025-03-23 22:38:32 +0100

    draw tiles from layers collection

--- walkers/src/map.rs
+++ walkers/src/map.rs
@@ -319,6 +319,10 @@ impl Widget for Map<'_, '_, '_> {
             draw_tiles(&painter, map_center, zoom, tiles);
         }
 
+        for tiles in self.layers {
+            draw_tiles(&painter, map_center, zoom, tiles);
+        }
+
         let projector = Projector::new(response.rect, self.memory, self.my_position);
         for (idx, plugin) in self.plugins.into_iter().enumerate() {
             let mut child_ui = ui.new_child(UiBuilder::new().max_rect(rect).id_salt(idx));
