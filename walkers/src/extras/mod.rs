9f4b5f18a20e0ff60a77a1629ec3a0ad12bdae79
Piotr Podusowski <podusowski@gmail.com>
2025-05-25 12:56:00 +0200

    image mod

--- walkers/src/extras/mod.rs
+++ walkers/src/extras/mod.rs
@@ -1,6 +1,9 @@
 //! Extra functionalities that can be used with the map.
+
+mod image;
 mod labeled_symbol;
 mod places;
 pub use crate::tiles::Texture;
+pub use image::Image;
 pub use labeled_symbol::{LabeledSymbol, LabeledSymbolStyle};
-pub use places::{Image, Places};
+pub use places::Places;
