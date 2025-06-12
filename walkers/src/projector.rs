e4e283caf16bb018ffce9464807466b12d0853b9
Piotr Podusowski <podusowski@gmail.com>
2025-06-12 18:05:12 +0200

    projector mod

---
+++ walkers/src/projector.rs
@@ -0,0 +1,126 @@
+use egui::{Rect, Vec2};
+
+use crate::{
+    mercator::{project, unproject},
+    position::{Pixels, PixelsExt as _},
+    MapMemory, Position,
+};
+
+/// Projects geographical position into pixels on the viewport, suitable for [`egui::Painter`].
+#[derive(Clone)]
+pub struct Projector {
+    clip_rect: Rect,
+    memory: MapMemory,
+    my_position: Position,
+}
+
+impl Projector {
+    pub fn new(clip_rect: Rect, map_memory: &MapMemory, my_position: Position) -> Self {
+        Self {
+            clip_rect,
+            memory: map_memory.to_owned(),
+            my_position,
+        }
+    }
+
+    /// Project `position` into pixels on the viewport.
+    pub fn project(&self, position: Position) -> Vec2 {
+        // Turn that into a flat, mercator projection.
+        let projected_position = project(position, self.memory.zoom());
+
+        // We need the precision of f64 here,
+        // since some "gaps" between tiles are noticeable on large zoom levels (e.g. 16+)
+        let zoom: f64 = self.memory.zoom();
+
+        // We also need to know where the map center is.
+        let map_center_projected_position = project(
+            self.memory.center_mode.position(self.my_position, zoom),
+            self.memory.zoom(),
+        );
+
+        // From the two points above we can calculate the actual point on the screen.
+        self.clip_rect.center().to_vec2()
+            + (projected_position - map_center_projected_position).to_vec2()
+    }
+
+    /// Get coordinates from viewport's pixels position
+    pub fn unproject(&self, position: Vec2) -> Position {
+        let zoom: f64 = self.memory.zoom();
+        let center = self.memory.center_mode.position(self.my_position, zoom);
+        let projected =
+            project(center, zoom).to_vec2() + position - self.clip_rect.center().to_vec2();
+
+        unproject(Pixels::new(projected.x as f64, projected.y as f64), zoom)
+    }
+
+    /// What is the local scale of the map at the provided position and given the current zoom
+    /// level?
+    pub fn scale_pixel_per_meter(&self, position: Position) -> f32 {
+        let zoom = self.memory.zoom();
+
+        // return f32 for ergonomics, as the result is typically used for egui code
+        calculate_meters_per_pixel(position.y(), zoom) as f32
+    }
+}
+
+/// Implementation of the scale computation.
+fn calculate_meters_per_pixel(latitude: f64, zoom: f64) -> f64 {
+    const EARTH_CIRCUMFERENCE: f64 = 40_075_016.686;
+
+    // Number of pixels for width of world at this zoom level
+    let total_pixels = crate::mercator::total_pixels(zoom);
+
+    let pixel_per_meter_equator = total_pixels / EARTH_CIRCUMFERENCE;
+    let latitude_rad = latitude.abs().to_radians();
+    pixel_per_meter_equator / latitude_rad.cos()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::lon_lat;
+    use egui::Pos2;
+
+    fn assert_approx_eq(a: f64, b: f64) {
+        let diff = (a - b).abs();
+        let tolerance = 0.01;
+        assert!(
+            diff < tolerance,
+            "Values differ by more than {tolerance}: {a} vs {b}"
+        );
+    }
+
+    #[test]
+    fn test_equator_zoom_0() {
+        // At zoom 0 (whole world), equator should be about 156.5km per pixel
+        let scale = calculate_meters_per_pixel(0.0, 0.);
+        assert_approx_eq(scale, 1. / 156_543.03);
+    }
+
+    #[test]
+    fn test_equator_zoom_19() {
+        // At max zoom (19), equator should be about 0.3m per pixel
+        let scale = calculate_meters_per_pixel(0.0, 19.);
+        assert_approx_eq(scale, 1. / 0.298);
+    }
+
+    #[test]
+    fn unproject_is_inverse_of_project() {
+        let original = lon_lat(21., 52.);
+
+        let mut map_memory = MapMemory::default();
+        map_memory.set_zoom(10.).unwrap();
+
+        let projector = Projector::new(
+            Rect::from_min_size(Pos2::ZERO, Vec2::splat(100.)),
+            &map_memory,
+            original,
+        );
+
+        let projected = projector.project(original);
+        let unprojected = projector.unproject(projected);
+
+        assert_approx_eq(original.x(), unprojected.x());
+        assert_approx_eq(original.y(), unprojected.y());
+    }
+}
