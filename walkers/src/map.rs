d73e12ed81a2a6b6d441ed867b2073c1c75e9991
Piotr Podusowski <podusowski@gmail.com>
2025-06-18 20:26:47 +0200

    clippy fix

--- walkers/src/map.rs
+++ walkers/src/map.rs
@@ -301,7 +301,7 @@ impl Widget for Map<'_, '_, '_> {
         let projector = Projector::new(response.rect, self.memory, self.my_position);
         for (idx, plugin) in self.plugins.into_iter().enumerate() {
             let mut child_ui = ui.new_child(UiBuilder::new().max_rect(rect).id_salt(idx));
-            plugin.run(&mut child_ui, &response, &projector, &self.memory);
+            plugin.run(&mut child_ui, &response, &projector, self.memory);
         }
 
         response
