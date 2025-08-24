8404bd78b9bac788230e25a7ab4aa847e74f25cb
Piotr Podusowski <podusowski@gmail.com>
2025-08-24 13:44:31 +0200

    cache

--- walkers/src/local_tiles.rs
+++ walkers/src/local_tiles.rs
@@ -1,28 +1,39 @@
+use crate::{sources::Attribution, Texture, TextureWithUv, TileId, Tiles};
+use lru::LruCache;
 use std::path::{Path, PathBuf};
-
-use crate::{sources::Attribution, Texture, TextureWithUv, Tiles};
 
 pub struct LocalTiles {
     path: PathBuf,
     egui_ctx: egui::Context,
+    cache: LruCache<TileId, Texture>,
 }
 
 impl LocalTiles {
     pub fn new(path: impl AsRef<Path>, egui_ctx: egui::Context) -> Self {
+        // Just arbitrary value which seemed right.
+        #[allow(clippy::unwrap_used)]
+        let cache_size = std::num::NonZeroUsize::new(256).unwrap();
+
         Self {
             path: path.as_ref().into(),
             egui_ctx,
+            cache: LruCache::new(cache_size),
         }
     }
 }
 
 impl Tiles for LocalTiles {
     fn at(&mut self, tile_id: crate::TileId) -> Option<crate::TextureWithUv> {
-        load(&self.path, tile_id, &self.egui_ctx)
-            .inspect_err(|err| {
-                log::warn!("Failed to load tile {:?}: {}", tile_id, err);
-            })
-            .ok()
+        let texture = self
+            .cache
+            .get(&tile_id)
+            .cloned()
+            .or_else(|| load(&self.path, tile_id, &self.egui_ctx))?;
+
+        Some(TextureWithUv {
+            texture,
+            uv: egui::Rect::from_min_max(egui::pos2(0.0, 0.0), egui::pos2(1.0, 1.0)),
+        })
     }
 
     fn attribution(&self) -> Attribution {
@@ -39,11 +50,19 @@ impl Tiles for LocalTiles {
     }
 }
 
-fn load(
+fn load(tiles_dir: &Path, tile_id: crate::TileId, egui_ctx: &egui::Context) -> Option<Texture> {
+    load_impl(tiles_dir, tile_id, egui_ctx)
+        .inspect_err(|err| {
+            log::warn!("Failed to load tile {:?}: {}", tile_id, err);
+        })
+        .ok()
+}
+
+fn load_impl(
     tiles_dir: &Path,
     tile_id: crate::TileId,
     egui_ctx: &egui::Context,
-) -> Result<TextureWithUv, Box<dyn std::error::Error>> {
+) -> Result<Texture, Box<dyn std::error::Error>> {
     let path = PathBuf::from_iter(&[
         tiles_dir.to_owned(),
         tile_id.zoom.to_string().into(),
@@ -51,8 +70,5 @@ fn load(
         format!("{}.png", tile_id.y).into(),
     ]);
     let bytes = std::fs::read(path)?;
-    Ok(TextureWithUv {
-        texture: Texture::new(&bytes, egui_ctx)?,
-        uv: egui::Rect::from_min_max(egui::pos2(0.0, 0.0), egui::pos2(1.0, 1.0)),
-    })
+    Ok(Texture::new(&bytes, egui_ctx)?)
 }
