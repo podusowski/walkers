77620bdd80b886d39d5d350a30c2cd91eaeba412
Piotr Podusowski <podusowski@gmail.com>
2025-10-31 21:55:09 +0100

    fix transform

--- walkers/src/mvt.rs
+++ walkers/src/mvt.rs
@@ -50,6 +50,7 @@ impl ShapeOrText {
                 shape.transform(transform);
             }
             ShapeOrText::Text(pos, _text) => {
+                *pos *= transform.scaling;
                 *pos += transform.translation;
             }
         }
