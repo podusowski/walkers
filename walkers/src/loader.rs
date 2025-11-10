b0753c1dfc9e3f958eed426b7faa8bf680444cd2
Piotr Podusowski <podusowski@gmail.com>
2025-11-10 21:34:10 +0100

    loader mod

---
+++ walkers/src/loader.rs
@@ -0,0 +1,56 @@
+/// Asynchronously load tiles from different local and remote sources.
+
+struct Loader {
+    /// Tiles to be downloaded by the IO thread.
+    request_tx: Sender<TileId>,
+
+    /// Tiles that got downloaded and should be put in the cache.
+    tile_rx: Receiver<(TileId, Texture)>,
+
+    cache: LruCache<TileId, Option<Texture>>,
+    http_stats: Arc<Mutex<HttpStats>>,
+
+    #[allow(dead_code)] // Significant Drop
+    runtime: Runtime,
+}
+
+impl Loader {
+    /// Construct new [`Tiles`] with supplied [`HttpOptions`].
+    pub fn with_options<S>(source: S, http_options: HttpOptions, egui_ctx: Context) -> Self
+    where
+        S: TileSource + Send + 'static,
+    {
+        let http_stats = Arc::new(Mutex::new(HttpStats { in_progress: 0 }));
+
+        // This ensures that newer requests are prioritized.
+        let channel_size = http_options.max_parallel_downloads.0;
+
+        let (request_tx, request_rx) = channel(channel_size);
+        let (tile_tx, tile_rx) = channel(channel_size);
+        let attribution = source.attribution();
+        let tile_size = source.tile_size();
+        let max_zoom = source.max_zoom();
+
+        // This will run concurrently in a loop, handing downloads and talk with us via channels.
+        let runtime = Runtime::new(download_continuously(
+            source,
+            http_options,
+            http_stats.clone(),
+            request_rx,
+            tile_tx,
+            egui_ctx,
+        ));
+
+        // Just arbitrary value which seemed right.
+        #[allow(clippy::unwrap_used)]
+        let cache_size = std::num::NonZeroUsize::new(256).unwrap();
+
+        Self {
+            cache: LruCache::new(cache_size),
+            http_stats,
+            request_tx,
+            tile_rx,
+            runtime,
+        }
+    }
+}
