a50abdae64973f9343928bdda1f7dbb8832b1364
Piotr Podusowski <podusowski@gmail.com>
2025-12-29 19:17:51 +0100

    use geo

--- walkers/src/text.rs
+++ walkers/src/text.rs
@@ -1,4 +1,5 @@
 use egui::{Color32, Pos2, Vec2, vec2};
+use geo::{BoundingRect, Intersects, bounding_rect};
 
 #[derive(Debug, Clone)]
 pub struct Text {
@@ -31,7 +32,8 @@ impl Text {
 }
 
 pub struct OrientedRect {
-    pub corners: [Pos2; 4],
+    pub geometry: geo::Polygon<f32>,
+    bbox: geo::Rect<f32>,
 }
 
 impl OrientedRect {
@@ -47,54 +49,39 @@ impl OrientedRect {
         let p2 = center + ux + uy; // bottom-right
         let p3 = center - ux + uy; // bottom-left
 
+        let polygon = geo::Polygon::new(
+            geo::LineString::from(vec![
+                geo::Coordinate { x: p0.x, y: p0.y },
+                geo::Coordinate { x: p1.x, y: p1.y },
+                geo::Coordinate { x: p2.x, y: p2.y },
+                geo::Coordinate { x: p3.x, y: p3.y },
+                geo::Coordinate { x: p0.x, y: p0.y }, // close the loop
+            ]),
+            vec![],
+        );
+
+        let bounding_rect = polygon.bounding_rect().unwrap();
+
         Self {
-            corners: [p0, p1, p2, p3],
+            geometry: polygon,
+            bbox: bounding_rect,
         }
     }
 
     pub fn top_left(&self) -> Pos2 {
-        self.corners[0]
+        self.geometry
+            .exterior()
+            .points()
+            .nth(0)
+            .map(|p| Pos2 {
+                x: p.x() as f32,
+                y: p.y() as f32,
+            })
+            .unwrap()
     }
 
     pub fn intersects(&self, other: &OrientedRect) -> bool {
-        // Separating Axis Theorem on the 4 candidate axes (2 from self, 2 from other)
-        for axis in self.edges().into_iter().chain(other.edges()) {
-            if axis.length_sq() == 0.0 {
-                continue; // degenerate, skip
-            }
-            let (a_min, a_max) = OrientedRect::project_onto_axis(&self.corners, axis);
-            let (b_min, b_max) = OrientedRect::project_onto_axis(&other.corners, axis);
-            // If intervals don't overlap -> separating axis exists
-            if a_max < b_min || b_max < a_min {
-                return false;
-            }
-        }
-        true
-    }
-
-    fn edges(&self) -> [egui::Vec2; 2] {
-        // Two unique edge directions are enough for SAT for rectangles.
-        [
-            self.corners[1] - self.corners[0],
-            self.corners[3] - self.corners[0],
-        ]
-    }
-
-    fn project_onto_axis(points: &[egui::Pos2; 4], axis: egui::Vec2) -> (f32, f32) {
-        // No need to normalize axis for interval overlap test
-        let dot = |p: egui::Pos2| -> f32 { p.x * axis.x + p.y * axis.y };
-        let mut min = f32::INFINITY;
-        let mut max = f32::NEG_INFINITY;
-        for &p in points {
-            let d = dot(p);
-            if d < min {
-                min = d;
-            }
-            if d > max {
-                max = d;
-            }
-        }
-        (min, max)
+        self.bbox.intersects(&other.bbox) && self.geometry.intersects(&other.geometry)
     }
 }
 
