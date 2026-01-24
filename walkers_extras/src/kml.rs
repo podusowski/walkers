b22ffc5d47d6466b46f6a32850e6ddee3a527fbe
Piotr Podusowski <podusowski@gmail.com>
2026-01-24 16:17:23 +0100

    kml circle layer

--- walkers_extras/src/kml.rs
+++ walkers_extras/src/kml.rs
@@ -3,7 +3,7 @@ use std::str::FromStr;
 use egui::{self, Color32, Response, Shape, Stroke, Ui};
 use kml::{KmlDocument, types::Folder};
 use log::{debug, warn};
-use walkers::{MapMemory, Plugin, Projector, Style, lon_lat};
+use walkers::{Layer, MapMemory, Plugin, Projector, Style, lon_lat};
 
 /// Plugin that renders parsed KML features on top of a [`Map`](walkers::Map).
 pub struct KmlLayer {
@@ -37,6 +37,32 @@ impl KmlLayer {
             | kml::Kml::Folder(Folder { elements, .. }) => {
                 for child in elements {
                     self.draw_line_layer(painter, response, projector, child);
+                }
+            }
+            _ => {
+                debug!("Skipping unsupported KML element: {:?}", element);
+            }
+        }
+    }
+
+    fn draw_circle_layer(
+        &self,
+        painter: &egui::Painter,
+        response: &Response,
+        projector: &Projector,
+        element: &kml::Kml,
+    ) {
+        match element {
+            kml::Kml::Placemark(placemark) => {
+                if let Some(geometry) = &placemark.geometry {
+                    self.draw_circle_geometry(&painter, response, projector, geometry);
+                }
+            }
+            kml::Kml::Document { elements, .. }
+            | kml::Kml::KmlDocument(KmlDocument { elements, .. })
+            | kml::Kml::Folder(Folder { elements, .. }) => {
+                for child in elements {
+                    self.draw_circle_layer(painter, response, projector, child);
                 }
             }
             _ => {
@@ -84,6 +110,34 @@ impl KmlLayer {
             _ => todo!(),
         }
     }
+
+    fn draw_circle_geometry(
+        &self,
+        painter: &egui::Painter,
+        response: &Response,
+        projector: &Projector,
+        geometry: &kml::types::Geometry,
+    ) {
+        match geometry {
+            kml::types::Geometry::Point(point) => {
+                let center = projector
+                    .project(lon_lat(point.coord.x, point.coord.y))
+                    .to_pos2();
+                let radius = 5.0;
+                let stroke = Stroke::new(1.0, Color32::BLACK);
+                let fill = Color32::from_rgb(0, 255, 0);
+
+                painter.add(Shape::circle_filled(center, radius, fill));
+                painter.add(Shape::circle_stroke(center, radius, stroke));
+            }
+            kml::types::Geometry::MultiGeometry(multi_geometry) => {
+                for geom in &multi_geometry.geometries {
+                    self.draw_circle_geometry(painter, response, projector, geom);
+                }
+            }
+            _ => todo!(),
+        }
+    }
 }
 
 impl Plugin for KmlLayer {
@@ -96,8 +150,16 @@ impl Plugin for KmlLayer {
     ) {
         for layer in &self.style.layers {
             match layer {
-                walkers::Layer::Line { .. } => {
+                Layer::Line { .. } => {
                     self.draw_line_layer(
+                        &ui.painter_at(response.rect),
+                        response,
+                        projector,
+                        &self.kml,
+                    );
+                }
+                Layer::Circle { .. } => {
+                    self.draw_circle_layer(
                         &ui.painter_at(response.rect),
                         response,
                         projector,
